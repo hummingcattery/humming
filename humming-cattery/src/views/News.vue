1198ac9c902e6455dbf4bfe8c53ab9730c7e5777
Tomi <ot_tomi@hotmail.com>
2024-02-17 00:59:53 +0200

    Add news 16.02.2024

--- humming-cattery/src/views/News.vue
+++ humming-cattery/src/views/News.vue
@@ -4,7 +4,7 @@
     </div>
 
     <div>
-        <p>Uutisista hyvää iltaa!</p>
+        <p>16.2.2024: Tervetuloa täysin uusituille kotisivuille! Meille on syntymässä pentuja helmikuun lopulla. Yhdistelmänä Hillevi x Copa (Demi JohnNatarie -E x Kyatwo T’Challa’s Glory). Tämän pentueen vanhemmat ovat ulkomaantuonteja ja suvullisesti verrattain hyvin kaukana toisistaan. Hillevi on tuotu Espanjasta kesällä 2022 ja Copa on tuotu Los Angelesista USA:sta kesällä 2023 (Copa on yhteisomistuksessa neljän kasvattajan kanssa: FI*Suvirannan, FI*Njaalan, FI*LeeviL & FI*Humming). Tällä yhdistelmällä on siis ensisijaisesti tarkoitus laajentaa birman geenipoolia Suomessa.</p>
     </div>
 
     <hr>
