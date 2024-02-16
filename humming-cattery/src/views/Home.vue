6924c60e2fbb4f79186bfd3049d07c6005374b1f
Tomi <ot_tomi@hotmail.com>
2024-02-17 00:48:35 +0200

    Update Home page header text

--- humming-cattery/src/views/Home.vue
+++ humming-cattery/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <h1>Tervetuloa Etusivulle!</h1>
+        <h1>Tervetuloa FI*Humming pyhä birma -kissalan sivuille!</h1>
     </div>
     <div>
         <img class="img-fluid" title="Unica vom Lilienweg" src="C:\Users\Tomi\Coding\FullStack\humming\humming-cattery\src\assets\images\Hulda_01.jpg" alt="Hulda" width="700" height="1000">
