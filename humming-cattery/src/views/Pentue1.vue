75386c8990869634a06c7eb28f36b57d6dbf0d40
Tomi <ot_tomi@hotmail.com>
2024-02-17 22:56:46 +0200

    Add picture to 1. pentue page

--- humming-cattery/src/views/Pentue1.vue
+++ humming-cattery/src/views/Pentue1.vue
@@ -1,32 +1,37 @@
 <template>
     <div>
-      <h1>1. pentue</h1>
+        <h1>1. pentue</h1>
     </div>
 
     <div class="content-container">
-      <h2>SC Zhamanen Harlequin DVM x Tamus Thunderbird</h2>
-      <p>Synt. 3.3.2013</p>
+        <h2>SC Zhamanen Harlequin DVM x Tamus Thunderbird</h2>
+        <p>Synt. 3.3.2013</p>
     </div>
     
     <h3>Pennut</h3>
     <table class="table table-bordered custom-table">
-    <tbody>
-      <tr>
-        <td>Humming Ummagumma</td>
-        <td>SBI b</td>
-        <td>uros</td>
-        <td>Näyttelytasoinen</td>
-        <td>Nominated for BIS</td>
-      </tr>
-      <tr>
-        <td>Humming High Hopes</td>
-        <td>SBI c</td>
-        <td>naaras</td>
-        <td>Siitostasoinen</td>
-        <td>BIS-winner</td>
-      </tr>
-    </tbody>
-  </table>
+        <tbody>
+        <tr>
+            <td>Humming Ummagumma</td>
+            <td>SBI b</td>
+            <td>uros</td>
+            <td>Näyttelytasoinen</td>
+            <td>Nominated for BIS</td>
+        </tr>
+        <tr>
+            <td>Humming High Hopes</td>
+            <td>SBI c</td>
+            <td>naaras</td>
+            <td>Siitostasoinen</td>
+            <td>BIS-winner</td>
+        </tr>
+        </tbody>
+    </table>
+
+    <div>
+        <img class="img-fluid" title="1. pentue" src="C:\Users\Tomi\Coding\FullStack\humming\humming-cattery\src\assets\images\pentue_01.jpg" alt="1. pentue" width="903" height="570">
+    </div>
+
 </template>
 
 <style lang="scss" scoped>
