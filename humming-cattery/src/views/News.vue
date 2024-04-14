4f6e78f7b073a63cd88cb796fc4f8929262d082b
Tomi <ot_tomi@hotmail.com>
2024-04-14 20:33:21 +0300

    Ádd news

--- humming-cattery/src/views/News.vue
+++ humming-cattery/src/views/News.vue
@@ -4,7 +4,26 @@
     </div>
 
     <div class="content-container">
-        <p>16.2.2024: Tervetuloa täysin uusituille kotisivuille! Meille on syntymässä pentuja helmikuun lopulla. Yhdistelmänä Hillevi x Copa (Demi JohnNatarie -E x Kyatwo T’Challa’s Glory). Tämän pentueen vanhemmat ovat ulkomaantuonteja ja suvullisesti verrattain hyvin kaukana toisistaan. Hillevi on tuotu Espanjasta kesällä 2022 ja Copa on tuotu Los Angelesista USA:sta kesällä 2023 (Copa on yhteisomistuksessa neljän kasvattajan kanssa: FI*Suvirannan, FI*Njaalan, FI*LeeviL & FI*Humming). Tällä yhdistelmällä on siis ensisijaisesti tarkoitus laajentaa birman geenipoolia Suomessa.</p>
+        <p><strong>14.4.2024</strong></p>
+        <p>
+        Hillevin ja Copan pennut syntyivät 26.2.2024. Kaksi ruskeanaamio-tyttöpentua, jotka etsivät molemmat sijoituskotia.<br>
+        Toinen tytöistä "Vilma" on näyttelytasoinen, joten kodilla olisi hyvä olla jonkinlainen innostus näyttelyistä.<br>
+        Toinen tytöistä "Valma" voisi kokeilla näyttelyitä pentuna, mutta toinen takakiila on vähän turhan niukka siihen, että näyttelyissä kerättäisiin aikuisena sertejä.<br>
+        Otathan yhteyttä sähköpostitse, jos olet kiinnostunut toimimaan sijoituskotina.
+        </p>
+    </div>
+    
+    <hr>
+
+    <div class="content-container">
+        <div>
+            <p><strong>16.2.2024</strong></p>
+            <p>
+            Tervetuloa täysin uusituille kotisivuille!<br>
+            Meille on syntymässä pentuja helmikuun lopulla. Yhdistelmänä Hillevi x Copa (Demi JohnNatarie -E x Kyatwo T’Challa’s Glory). Tämän pentueen vanhemmat ovat ulkomaantuonteja ja suvullisesti verrattain hyvin kaukana toisistaan.<br>
+            Hillevi on tuotu Espanjasta kesällä 2022 ja Copa on tuotu Los Angelesista USA:sta kesällä 2023 (Copa on yhteisomistuksessa neljän kasvattajan kanssa: FI*Suvirannan, FI*Njaalan, FI*LeeviL & FI*Humming). Tällä yhdistelmällä on siis ensisijaisesti tarkoitus laajentaa birman geenipoolia Suomessa.
+            </p>
+        </div>
     </div>
 
     <hr>
