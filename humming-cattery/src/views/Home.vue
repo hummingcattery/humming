8c5f805622b210a51543efeb9f51768fb3177c26
Tomi <ot_tomi@hotmail.com>
2024-04-15 18:56:34 +0300

    Remove etusivu picture

--- humming-cattery/src/views/Home.vue
+++ humming-cattery/src/views/Home.vue
@@ -7,7 +7,7 @@
             Meillä on pentuja, etsinnässä kaksi eri sijoituskotia PK-seudulta tai sopimuksen mukaan myös esim. Pirkanmaalta. Mahdollista myös sijoittaa molemmat tytöt samaan kotiin.<br>
             Lue sijoitusehdoista Kissalan Pentu-infosta.
         </p>
-        <img class="img-fluid" src="C:\Users\Tomi\Coding\FullStack\humming\humming-cattery\src\assets\images\Etusivu.jpg" width="700" height="1000">
+        <!-- <img class="img-fluid" src="C:\Users\Tomi\Coding\FullStack\humming\humming-cattery\src\assets\images\Etusivu.jpg" width="700" height="1000"> -->
     </div>
 </template>
 
